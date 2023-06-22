d8608d5edb01e964f21ec63553f2411e5bd033c9
Lucia Lisdero <lulisdero20@gmail.com>
2023-06-21 23:23:52 -0300

    realstate

--- backend/src/models/RealState.js
+++ backend/src/models/RealState.js
@@ -5,28 +5,117 @@ module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('RealState', {
     id: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
       primaryKey: true,
-      autoIncrement: true
     },
-    mail: {
+    adress: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    age: {
+    banios: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    anios: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    descripcion: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    mail: {
+    disposicion: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+    expensas: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    plantas: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    alquilertemporal: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    ubicacion: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    password: {
+    operacion: {
       type: DataTypes.STRING,
       allowNull: false,
     },
-    rol: {
+    orientacion: {
       type: DataTypes.STRING,
+      allowNull: false,
+    },
+    estacionamiento: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    fotos: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    direccionreal: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    condiciondelinmueble: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    superficiecubierta: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    habitaciones: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    superficiesemicubierta: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    situacion: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    dormitorios: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    superficie: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    etiquetas: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
+      allowNull: false,
+    },
+    toilettes: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    superficietotal: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    tipo: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    descubierto: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    videos: {
+      type: DataTypes.ARRAY(DataTypes.STRING),
       allowNull: true,
     }
   });
