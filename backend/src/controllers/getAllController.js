056f17d236af2695cb34611d91cfa20a7697a679
NicoAlonso <nicoalonso99.na@gmail.com>
2023-06-23 15:30:17 -0300

    lol

--- backend/src/controllers/getAllController.js
+++ backend/src/controllers/getAllController.js
@@ -1,13 +1,12 @@
 const axios = require('axios');
-// const { cleanData } = require('../Helpers/cleanData');
-const { RealState } = require('../db')
+const { RealState } = require('../db');
 
 
 const getRealState = async () => {
   const allRealEstates = await RealState.findAll()
 
   return allRealEstates
-}
+};
 
 module.exports = {
   getRealState
