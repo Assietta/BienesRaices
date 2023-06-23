61e1a3bd8d287bedc88eef15598db9605a49619a
Franco Galarza <francig4444@gmail.com>
2023-06-22 22:36:14 -0300

    Delete User

---
+++ backend/src/handlers/deleteUserHandler.js
@@ -0,0 +1,16 @@
+const deleteUserController = require('../controllers/deleteUserController')
+
+const deleteUserHandler = async(req, res) => {
+    try {
+       const { id } = req.params
+
+        await deleteUserController(id)
+
+        return res.status(200).send('User eliminated')
+    } catch (error) {
+        return res.status(500).send('User cannot be eliminated')
+    }
+    
+}
+
+module.exports = deleteUserHandler
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,14 +6,15 @@ const { realStateHandler }= require('../handlers/getAll');
 const { postRealStateHandler }= require('../handlers/postRealState');
 const { getUsersHandler } = require('../handlers/getUsers')
 const postUserHandler = require('../handlers/postUserHandler');
-
+const deleteUserHandler = require('../handlers/deleteUserHandler')
 
 
 router.get('/realState', realStateHandler);
 router.get('/realState/:id', getPropertyIdHandler)
 router.post('/realStatePost', postRealStateHandler);
 router.post('/users', postUserHandler);
-router.get('/users', getUsersHandler)
+router.get('/users', getUsersHandler);
+router.delete('/users/:id', deleteUserHandler)
 
 module.exports = router;
 
