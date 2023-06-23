6f69741ed3636ad3e73cb8f5caf2c488ede0c2dd
NicoAlonso <nicoalonso99.na@gmail.com>
2023-06-23 13:26:55 -0300

    Back arreglado

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,19 +1,21 @@
 const { Router } = require('express');
 const router = Router();
 const getPropertyIdHandler = require('../handlers/getPropertyIdHandler');
+const { putUsersHandler } = require('../handlers/putUsersHandler');
 
-const { realStateHandler }= require('../handlers/getAll');
-const { postRealStateHandler }= require('../handlers/postRealState');
-const { getUsersHandler } = require('../handlers/getUsers')
+const { realStateHandler }= require('../handlers/getAllHandler');
+const { postRealStateHandler }= require('../handlers/postRealStateHandler');
+const { getUsersHandler } = require('../handlers/getUsersHandler')
 const postUserHandler = require('../handlers/postUserHandler');
 
 
 
 router.get('/realState', realStateHandler);
-router.get('/realState/:id', getPropertyIdHandler)
-router.post('/realStatePost', postRealStateHandler);
+router.get('/realState/:id', getPropertyIdHandler);
+router.post('/realState', postRealStateHandler);
 router.post('/users', postUserHandler);
-router.get('/users', getUsersHandler)
+router.get('/users', getUsersHandler);
+router.put('/users/:id', putUsersHandler);
 
 module.exports = router;
 
