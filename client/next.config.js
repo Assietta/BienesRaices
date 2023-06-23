7a100d04badbab3625cddaf10f980095cf839755
EzeZungri99 <zungri2020@gmail.com>
2023-06-23 19:37:51 -0300

    carrousel y footer

--- client/next.config.js
+++ client/next.config.js
@@ -1,4 +1,9 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images: {
+        domains: ['i.blogs.es', 'st.depositphotos.com', "i.ibb.co"],
+      }
+
+}
 
 module.exports = nextConfig
---
+++ client/src/app/About/page.jsx
@@ -0,0 +1,6 @@
+
+export default function Hola (){
+    return(
+        <div>hola maestro</div>
+    )
+  }
