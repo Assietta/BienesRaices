7da22890ecbbda321b9f15f68ba589025e4e2a85
NicoAlonso <nicoalonso99.na@gmail.com>
2023-06-23 15:23:26 -0300

    lol

--- backend/src/controllers/getAllRealEstateController.js
+++ backend/src/controllers/getAllRealEstateController.js
@@ -7,15 +7,6 @@ const { RealState } = require('../db')
 const getAllApi = async() => {
     let apiInfo = []
 
-    // const URL = 'https://www.tokkobroker.com'
-    // let next = `/api/v1/property/?lang=es_ar&format=json&limit=20&offset=0&key=${API_KEY}`
-
-    // while (next !== null) {
-    //     const { data } = await axios(URL + next)   
-    //     apiInfo = [...apiInfo, ...cleanData(data)]
-    //     next = data.meta.next
-    // }
-
     const URL = `https://www.tokkobroker.com/api/v1/property/?lang=es_ar&format=json&limit=60&offset=0&key=${API_KEY}`
     const { data } = await axios(URL)
     apiInfo = cleanData(data)
