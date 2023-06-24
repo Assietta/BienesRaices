49eb0162f162008025c474af1505482ff1e8f272
Lucia Lisdero <lulisdero20@gmail.com>
2023-06-24 20:07:28 -0300

    models

---
+++ backend/src/models/Contact.js
@@ -0,0 +1,31 @@
+const { DataTypes } = require('sequelize');
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define('Contact', {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true
+    },
+    username: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true
+    },
+    password: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    rol: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+  });
+};
