dbc62ed624823efb0e81647ce2d90c964264d1a3
EzeZungri99 <zungri2020@gmail.com>
2023-06-24 16:31:01 -0300

    primeras 3 action

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -1,17 +1,46 @@
 export const GET_PROP = "GET_PROP"
-
+export const GET_PROP_BY_ID = "GET_PROP_BY_ID"
+export const POST_PROP_BY_ID ="POST_PROP_BY_ID"
 
 export function getAllProperties(){
     return async function (dispatch){
         try {
             const response = await axios.get("/realState");
-            console.log(response);
+            
             dispatch({
                 type: GET_PROP,
                 payload: response.data
             })
         } catch (error) {
             console.error("Error while getting realStateHandler", error);
+        }
+    }
+}
+
+export function getPropById(id) {
+    return async function (dispatch){
+        try {
+            const response = await axios.get(`/realState/${id}`)
+           return  dispatch({
+                type: GET_PROP_BY_ID,
+                payload: response.data
+            })
+        } catch (error) {
+            console.error("Error while getting Get Prop By Id", error);
+        }
+    }
+}
+
+export function postProp() {
+    return async function (dispatch){
+        try {
+            const response = await axios.get(`/realState`)
+           return  dispatch({
+                type: POST_PROP_BY_ID,
+                payload: response.data
+            })
+        } catch (error) {
+            console.error("Error while getting Get Prop By Id", error);
         }
     }
 }
