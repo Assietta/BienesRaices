fb94eee6a79923763391dfabd92b13202a378836
Lucia Lisdero <lulisdero20@gmail.com>
2023-06-26 13:49:38 -0300

    db y order.js model

---
+++ backend/src/models/Order.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+// Exportamos una funcion que define el modelo
+// Luego le injectamos la conexion a sequelize.
+module.exports = (sequelize) => {
+  // defino el modelo
+  sequelize.define("Appraisal", {
+    id: {
+      type: DataTypes.UUID,
+      defaultValue: DataTypes.UUIDV4,
+      allowNull: false,
+      primaryKey: true,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    lastName: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      unique: true,
+    },
+    mobile: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    payment: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+  });
+};
