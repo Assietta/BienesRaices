15f7a3379a453b9ea0b1b2a358afcdc3d76e7571
EzeZungri99 <zungri2020@gmail.com>
2023-06-26 17:47:09 -0300

    forms css

--- client/src/app/About/page.jsx
+++ client/src/app/About/page.jsx
@@ -30,7 +30,7 @@ export default function Example() {
     <div className="bg-white py-24 sm:py-32">
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
         <div className="mx-auto max-w-2xl lg:text-center">
-<h2 className="text-base font-semibold leading-7 text-indigo-600">
+        <h2 className="text-base font-semibold leading-7 text-indigo-600">
             Nosotros
           </h2>
           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">
