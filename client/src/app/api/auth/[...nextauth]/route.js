98dc77f08309e73529cd77d23bb72a17ec8ebc99
claymartinez <clay.ms@hotmail.com>
2023-06-30 11:55:07 -0500

    route

--- client/src/app/api/auth/[...nextauth]/route.js
+++ client/src/app/api/auth/[...nextauth]/route.js
@@ -14,14 +14,6 @@ const handler = NextAuth({
     }),
     // Puedes agregar más proveedores aquí si lo deseas
   ],
-
-  // pages: {
-  //   signIn: '/auth/signin',
-  //   signOut: '/auth/signout',
-  //   error: '/auth/error', // Error code passed in query string as ?error=
-  //   verifyRequest: '/auth/verify-request', // (used for check email message)
-  //   newUser: '/auth/new-user', // New users will be directed here on first sign in (leave the property out if not of interest)
-  // },
 });
 
 export { handler as GET, handler as POST };
