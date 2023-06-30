e13c09a87fd4e2b20d4d6ab5a4b3a1adc298d1b7
NicoArgizz <argiz.n@gmail.com>
2023-06-30 15:48:18 -0300

    navbar y footer

--- backend/src/Helpers/cleanData.js
+++ backend/src/Helpers/cleanData.js
@@ -1,5 +1,3 @@
-
-
 const cleanData = (data) => {
   const objects = data.objects;
   const cleanedData = [];
@@ -7,36 +5,36 @@ const cleanData = (data) => {
   for (let i = 0; i < objects.length; i++) {
     const object = objects[i];
 
-    const address = object.fake_address || '';
+    const address = object.fake_address || "";
     const bathroom_amount = object.bathroom_amount || 0;
     const age = object.age || 0;
-    const description = object.description || '';
-    const disposition = object.disposition || '';
+    const description = object.description || "";
+    const disposition = object.disposition || "";
     const expenses = object.expenses || 0;
     const floors_amount = object.floors_amount || 0;
     const has_temporary_rent = object.has_temporary_rent;
-    const location = object.short_location || '';
+    const location = object.short_location || "";
     const operations = object.operations || {};
-    const orientation = object.orientation || '';
+    const orientation = object.orientation || "";
     const parking_lot_amount = object.parking_lot_amount || 0;
-    const photos = object.photos?.map(photo => photo.image) || [''];
-    const real_address = object.real_address || '';
-    const property_condition = object.property_condition || '';
-    const roofed_surface = object.roofed_surface || '';
+    const photos = object.photos?.map((photo) => photo.image) || [""];
+    const real_address = object.real_address || "";
+    const property_condition = object.property_condition || "";
+    const roofed_surface = object.roofed_surface || "";
     const room_amount = object.room_amount || 0;
-    const semiroofed_surface = object.semiroofed_surface || '';
-    const situation = object.situation || '';
+    const semiroofed_surface = object.semiroofed_surface || "";
+    const situation = object.situation || "";
     const suite_amount = object.suite_amount || 0;
-    const surface = object.surface || '';
+    const surface = object.surface || "";
     const tags = object.tags || [{}];
     const toilet_amount = object.toilet_amount || 0;
-    const total_surface = object.total_surface || '';
-    const type = object.type?.name || '';
-    const unroofed_surface = object.unroofed_surface || '';
-    const videos = object.videos?.map(video => video.url) || [''];
-    const geo_lat = object.geo_lat || '';
-    const geo_long = object.geo_long || '';
-    
+    const total_surface = object.total_surface || "";
+    const type = object.type?.name || "";
+    const unroofed_surface = object.unroofed_surface || "";
+    const videos = object.videos?.map((video) => video.url) || [""];
+    const geo_lat = object.geo_lat || "";
+    const geo_long = object.geo_long || "";
+
     const cleanedObject = {
       address,
       real_address,
@@ -68,13 +66,13 @@ const cleanData = (data) => {
       geo_lat,
       geo_long,
     };
-    
+
     cleanedData.push(cleanedObject);
   }
-  
+
   return cleanedData;
 };
 
 module.exports = {
-  cleanData
+  cleanData,
 };
