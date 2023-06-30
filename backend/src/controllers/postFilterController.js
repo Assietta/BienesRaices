49a4c860e07d4e20b2c3b6a12e442bd18823274f
Jeremias Brussino <jeremiasbrussino@gmail.com>
2023-06-30 14:12:17 -0300

    actu

--- backend/src/controllers/postFilterController.js
+++ backend/src/controllers/postFilterController.js
@@ -1,7 +1,7 @@
 const { RealState } = require('../db');
 const { Op } = require('sequelize');
 
-const postFilterController = async (type, operation_type, maxPrice, minPrice, orientation, currency, tags) => {
+const postFilterController = async (type, operation_type, maxPrice, minPrice, orientation, currency, bathroom_amount, parking_lot_amount, room_amount, suite_amount, tags) => {
   const filters = {};
   if (type) {
     filters.type = type;
@@ -26,6 +26,21 @@ const postFilterController = async (type, operation_type, maxPrice, minPrice, or
       [Op.lte]: maxPrice
     };
   }
+                    if (bathroom_amount) {
+                      filters.bathroom_amount = bathroom_amount;
+                    }
+                    if (parking_lot_amount) {
+                      filters.parking_lot_amount = parking_lot_amount;
+                    }
+                    if (room_amount) {
+                      filters.room_amount = room_amount;
+                    }
+                    if (suite_amount) {
+                      filters.suite_amount = suite_amount;
+                    }
+
+
+
   if (tags && tags.length > 0) {
     filters.tags = {
       [Op.contains]: tags
--- backend/src/handlers/postFilterHandler.js
+++ backend/src/handlers/postFilterHandler.js
@@ -1,9 +1,9 @@
 const { postFilterController } = require('../controllers/postFilterController');
 
 const postFilterHandler = async (req, res) => {
-  const { type, operation_type, maxPrice, minPrice, orientation,  currency, tags} = req.body;
+  const { type, operation_type, maxPrice, minPrice, orientation,  currency, bathroom_amount, parking_lot_amount, room_amount, suite_amount, tags} = req.body;
   try {
-    const properties = await postFilterController(type, operation_type, maxPrice, minPrice, orientation,  currency, tags)
+    const properties = await postFilterController(type, operation_type, maxPrice, minPrice, orientation,  currency, bathroom_amount,parking_lot_amount, room_amount, suite_amount, tags)
     res.json(properties);
   } catch (error) {
     res.status(500).json({ error: error.message });
