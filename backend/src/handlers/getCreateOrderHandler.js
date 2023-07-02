bd8878538d26fff8ab344f033f09da09c3a8e977
NicoArgizz <argiz.n@gmail.com>
2023-07-02 15:48:56 -0300

    detail, card y cardcontainer

--- backend/src/handlers/getCreateOrderHandler.js
+++ backend/src/handlers/getCreateOrderHandler.js
@@ -9,10 +9,8 @@ const createOrderHandler = async (req, res) => {
   });
 
   try {
-    const property = await RealState.findOne({   where: { id: id }});
-    const price= parseFloat(property?.operations[0]?.prices[0]?.price) * 0.001
-;
-
+    const property = await RealState.findOne({ where: { id: id } });
+    const price = parseFloat(property?.operations[0]?.prices[0]?.price) * 0.001;
     const result = await mercadopago.preferences.create({
       items: [
         {
@@ -22,11 +20,9 @@ const createOrderHandler = async (req, res) => {
           unit_price: price,
         },
       ],
-
     });
 
     const preferenceId = result.response.id;
-   
 
     // const handleReservarClick = async () => {
     //   try {
@@ -39,9 +35,7 @@ const createOrderHandler = async (req, res) => {
     //     // Manejar el error de acuerdo a tus necesidades
     //   }
     // };
-  
 
-    
     res.send(preferenceId);
     // res.redirect(redirectUrl);
   } catch (error) {
