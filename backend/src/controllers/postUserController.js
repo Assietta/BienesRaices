e1630fba3ede4639fb1ef51c2bfbae026d474dd4
nicoargiz <nicoargiz@hotmail.com>
2023-07-05 16:06:23 -0300

    arreglado?

--- backend/src/controllers/postUserController.js
+++ backend/src/controllers/postUserController.js
@@ -4,7 +4,7 @@ const bcrypt = require('bcryptjs');
 const postUserController = async(email, password, username, rol, image, provider) => {
     const saltRounds = 10;
     if(password) password = await bcrypt.hash(password, saltRounds);
-    const user = await User.findOrCreate({email, password, username, rol, image, provider}, {timestamps: false})
+    const user = await User.findOrCreate({where:{email, password, username, rol, image, provider}}, {timestamps: false})
 
     return user
 }
