27c63c9127ec9cc73f244d6e67babdffe01cabae
nicoargiz <nicoargiz@hotmail.com>
2023-07-05 16:10:49 -0300

    lol ahora si?

--- backend/src/controllers/postUserController.js
+++ backend/src/controllers/postUserController.js
@@ -1,13 +1,21 @@
-const { User } = require("../db")
+const { User } = require("../db");
 const bcrypt = require('bcryptjs');
 
-const postUserController = async(email, password, username, rol, image, provider) => {
-    const saltRounds = 10;
-    if(password) password = await bcrypt.hash(password, saltRounds);
-    const user = await User.findOrCreate({where:{email, password, username, rol, image, provider}}, {timestamps: false})
+const postUserController = async (email, password, username, rol, image, provider) => {
+  const saltRounds = 10;
+  if (password) password = await bcrypt.hash(password, saltRounds);
+  const user = await User.findOrCreate({
+    where: { email },
+    defaults: { 
+      password,
+      username,
+      rol,
+      image,
+      provider
+    }
+  });
 
-    return user
-}
+  return user;
+};
 
-
-module.exports = postUserController
+module.exports = postUserController;
