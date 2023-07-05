e13adcd9207ac698359982b95d50bd9f7f07b59d
Lucia Lisdero <lulisdero20@gmail.com>
2023-07-05 15:00:51 -0300

    guardar orden de pago

--- backend/src/handlers/postCreateOrderHandler.js
+++ backend/src/handlers/postCreateOrderHandler.js
@@ -70,16 +70,74 @@ const webhookHandler=async(req, res)=>{
     if(payment.type==="payment"){
       const data=await mercadopago.payment.findById(payment["data.id"]);
     console.log(data);
-    //BUSCAR USER EN DB Y GUARDAR REGISTRO EN ORDERS
+
+      // BUSCAR USER EN DB
+      const user = await User.findByPk(data.external_reference); // Asegúrate de que el modelo User exista y esté configurado correctamente
+
+      // BUSCAR PROPERTY EN DB
+      const property = await RealState.findByPk(data.description); // Asegúrate de que el modelo RealState exista y esté configurado correctamente
+
+      // GUARDAR REGISTRO EN ORDERS
+      const newOrder = await Order.create({
+        client_idDB: data.external_reference,
+        usernameDB: user.username,
+        property_idDB: data.description,
+        address_of_property_to_reserveDB: property.address,
+        date_created: data.date_created,
+        chain: data.currency_id,
+        transaction_amount: data.transaction_amount,
+        date_approved: data.date_approved,
+        payment_id: data.id,
+        payer_first_nameMP: data.payer.first_name,
+        payer_last_nameMP: data.payer.last_name,
+        payer_emailMP: data.payer.email,
+        payer_emailDB: user.email,
+        payer_identificationMP: data.payer.identification,
+        payer_phoneMP: data.payer.phone,
+        payer_idMP: data.payer.id,
+        payment_method_id: data.payment_method_id,
+        payment_type_id: data.payment_type_id,
+        statement_descriptor_usernameMP: data.statement_descriptor,
+        status_approved_rejected: data.status,
+        status_detail_accredited: data.status_detail,
+        taxes_amount: data.taxes_amount,
+        transaction_amount_refunded: data.transaction_amount_refunded
+      });
+
+      console.log(newOrder);
+
+
     //NOTIFICACION POR MAIL
+
     res.status(200).send("OK");
     }
   } catch (error) {
     return res.sendStatus(500).json({error: error.message});
   }
 };
+
+
+
+module.exports = { createOrderHandler , webhookHandler};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
   // try {
-  
   //   //   const data= await  axios.get(`https://api.mercadopago.com/v1/payments/${payment}`, {
   //   //     headers: {
   //   //         'Authorization': `Bearer TEST-4304274978344220-062414-3619cd7e5c484d5fcb746d26d1cc68c0-1406402853`
@@ -98,10 +156,17 @@ const webhookHandler=async(req, res)=>{
   // }
 
 
+
+
+
+
+
+
+
 const approved =async(req, res)=>{
-  axios.get(`https://api.mercadopago.com/v1/payments/${paymentdId}`, {
+  axios.get(`https://api.mercadopago.com/v1/payments/${payment["data.id"]}}`, {
     headers: {
-        'Authorization': `Bearer TEST-4304274978344220-062414-3619cd7e5c484d5fcb746d26d1cc68c0-1406402853`
+        'Authorization': TOKEN_MP
     }
 }).then(response => {
     console.log(response.data);
@@ -110,4 +175,4 @@ const approved =async(req, res)=>{
 });}
 
 
-module.exports = { createOrderHandler , webhookHandler};
+
