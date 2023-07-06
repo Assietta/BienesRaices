550220116d4dfdf89df0f4414a0282b24441b804
EzeZungri99 <zungri2020@gmail.com>
2023-07-06 13:24:23 -0300

    actual

--- backend/src/handlers/postUserHandler.js
+++ backend/src/handlers/postUserHandler.js
@@ -5,10 +5,10 @@ const postUserHandler = async(req, res) => {
     try {
         const { email, password, username, rol, image, provider } = req.body
         const userPosted = await postUserController(email, password, username, rol, image, provider) ;
-        const asunto = 'Bienvenido a Henry Real State';
-        const destinatario = email;
-        const cuerpo = 'Gracias por registrarte en nuestro sitio.'
-        await mailHandler(destinatario, asunto, cuerpo);
+        // const asunto = 'Bienvenido a Henry Real State';
+        // const destinatario = email;
+        // const cuerpo = 'Gracias por registrarte en nuestro sitio.'
+        // await mailHandler(destinatario, asunto, cuerpo);
         return res.status(200).json(userPosted)
     } catch (error) {
         return res.status(500).json({error: error.message})
