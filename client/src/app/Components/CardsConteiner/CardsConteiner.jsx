7105243e1b6dd9a2d29e4cee323e3f884814407d
Lulisderoo20 <92049842+Lulisderoo20@users.noreply.github.com>
2023-07-06 15:16:37 -0300

    Update CardsConteiner.jsx

--- client/src/app/Components/CardsConteiner/CardsConteiner.jsx
+++ client/src/app/Components/CardsConteiner/CardsConteiner.jsx
@@ -1,44 +1,152 @@
+"use client";
 import React, { useEffect, useState } from "react";
 import style from "./CardsConteiner.module.css";
+import SearchBar from "../SearchBar/SearchBar";
 import Card from "../Card/Card";
-import axios from 'axios';
+import { useDispatch, useSelector } from "react-redux";
+import {
+  getAllProperties,
+  orderByOperation,
+  orderByOrientation,
+  orderByFloor,
+  OrderByPrice,
+} from "../../../redux/actions";
+
 
 const CardsConteiner = () => {
-  const [viewProps, setViewProps] = useState([]);
+  const propsGlobal = useSelector((state) => state.allProps);
+  const filterProps = useSelector((state) => state.allPropsCopy);
+
+  const itemsPerPage = 3;
   const [currentPage, setCurrentPage] = useState(1);
 
-  useEffect(() => {
-    fetchData();
-  }, [currentPage]);
+  const [allProps, setProps] = useState([]);
 
-  const fetchData = async () => {
-    try {
-      const response = await axios.get(`http://localhost:3001/realstate?page=${currentPage}&limit=3`);
-      const data = response.data;
-      console.log(data);
-      setViewProps(data);
-    } catch (error) {
-      // Manejar el error de la solicitud
-      console.error(error);
-    }
+  const indexOfLastCountry = currentPage * itemsPerPage;
+  const indexOfFirstCountry = indexOfLastCountry - itemsPerPage;
+  const viewProps = allProps.slice(indexOfFirstCountry, indexOfLastCountry);
+
+  const goToPreviousPage = () => {
   };
 
-  const randomImageIndex = Math.floor(Math.random() * 5);
-
-  const handleNextPage = () => {
+  const goToNextPage = () => {
     setCurrentPage((prevPage) => prevPage + 1);
   };
 
-  const handlePreviousPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage((prevPage) => prevPage - 1);
-    }
-  };
+  const dispatch = useDispatch();
 
+  useEffect(() => {
+    if (propsGlobal.length < 1) {
+      dispatch(getAllProperties());
+    } else {
+      setProps(propsGlobal);
+    }
+  }, [propsGlobal]);
+
+  // const filterByPrice = (e) => {
+  //   dispatch(OrderByPrice(e.target.value));
+
+  //   if (e.target.value === "All") {
+  //     setProps([...allProps]);
+  //   } else {
+  //     setProps([...filterProps]);
+  //   }
+  //   e.target.value = "";
+  // };
+
+  // const filterByOrientation = (e) => {
+  //   dispatch(orderByOrientation(e.target.value));
+
+  //   if (e.target.value === "All") {
+  //     setProps([...propsGlobal]);
+  //   } else {
+  //     const filteredProps = propsGlobal.filter(
+  //       (prop) => prop.orientation === e.target.value
+  //     );
+  //     setProps([...filteredProps]);
+  //   }
+  //   setCurrentPage(1); // Reinicia la página actual después de aplicar el filtro
+  // };
+
+  // const filterByOperation = (e) => {
+  //   dispatch(orderByOperation(e.target.value));
+
+  //   if (e.target.value === "All") {
+  //     setProps([...propsGlobal]);
+  //   } else {
+  //     const filteredProps = propsGlobal.filter(
+  //       (prop) => prop.operations[0]?.operation_type === e.target.value
+  //     );
+  //     setProps([...filteredProps]);
+  //   }
+  //   setCurrentPage(1); // Reinicia la página actual después de aplicar el filtro
+  // };
+
+  // const filterByFloor = (e) => {
+  //   dispatch(orderByFloor(e.target.value));
+
+  //   if (e.target.value === "") {
+  //     setProps([...propsGlobal]);
+  //   } else {
+  //     const filteredProps = propsGlobal.filter(
+  //       (prop) => prop.floors_amount === Number(e.target.value)
+  //     );
+  //     setProps([...filteredProps]);
+  //   }
+  //   setCurrentPage(1); // Reinicia la página actual después de aplicar el filtro
+  // };
+  const randomImageIndex = Math.floor(Math.random() * 5);
   return (
     <div>
+      {/* <div className={style.filtersContainer}>
+        <div className={style.filters}>
+          <select className={style.selects} onChange={filterByOrientation}>
+            <option value="" hidden>
+              Orientation
+            </option>
+            <option value="All">All</option>
+            <option value="Noreste">Noreste</option>
+            <option value="Noroeste">Noroeste</option>
+            <option value="Este">Este</option>
+            <option value="Oeste">Oeste</option>
+            <option value="Sur">Sur</option>
+            <option value="Suroeste">Suroeste</option>
+            <option value="Sureste">Sureste</option>
+          </select>
+          <select className={style.selects} onChange={filterByOperation}>
+            <option value="" hidden>
+              Operation
+            </option>
+            <option value="All">All</option>
+            <option value="Venta">Venta</option>
+            <option value="Alquiler">Alquiler</option>
+          </select>
+          <select className={style.selects} onChange={filterByFloor}>
+            <option value="" hidden>
+              Floors
+            </option>
+            <option value="0">0</option>
+            <option value="1">1</option>
+            <option value="2">2</option>
+            <option value="3">3</option>
+          </select>
+          <select className={style.selects} onChange={filterByPrice}>
+            <option value="" hidden>
+              Price
+            </option>
+            <option value="All">All</option>
+            <option value="150000000">150.000.000</option>
+            <option value="100000000">100.000.000</option>
+            <option value="50000000">50.000.000</option>
+            <option value="10000000">10.000.000</option>
+            <option value="1000000">1.000.000</option>
+          </select> 
+        </div>
+      </div> */}
+
       <h1 className={style.title}>Propiedades Destacadas</h1>
       <div className={style.container}>
+        {/* Card */}
         {viewProps.map((prop) => (
           <Card
             key={prop.id}
@@ -59,11 +167,59 @@ const CardsConteiner = () => {
           />
         ))}
       </div>
-      <div className={style.pagination}>
-        <button onClick={handlePreviousPage} disabled={currentPage === 1}>
-          Anterior
-        </button>
-        <button onClick={handleNextPage}>Siguiente</button>
+      <div>
+        {/* Paginación */}
+        <div className={style.pagination}>
+          {/* Botón de retroceso */}
+          <li className={style.il}>
+            <button
+              onClick={goToPreviousPage}
+              disabled={currentPage === 1}
+              className={`h-10 px-5 text-green-600 transition-colors duration-150 rounded-l-lg focus:shadow-outline hover:bg-green-100 ${
+                currentPage === 1 ? "hidden" : ""
+              }`}
+            >
+              <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                <path
+                  d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+          </li>
+
+          {/* Número de página actual */}
+
+          <li className={style.il}>
+            <button className="h-10 px-5 text-white transition-colors duration-150 bg-green-600 border border-r-0 border-green-600 focus:shadow-outline">
+              {currentPage}
+            </button>
+          </li>
+
+          {/* Botón de avance */}
+          <li className={style.il}>
+            <button
+              onClick={goToNextPage}
+              disabled={
+                currentPage === Math.ceil(allProps.length / itemsPerPage)
+              }
+              className={`h-10 px-5 text-green-600 transition-colors duration-150 rounded-r-lg focus:shadow-outline hover:bg-green-100 ${
+                currentPage === Math.ceil(allProps.length / itemsPerPage)
+                  ? "hidden"
+                  : ""
+              }`}
+            >
+              <svg className="w-4 h-4 fill-current" viewBox="0 0 20 20">
+                <path
+                  d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
+                  clipRule="evenodd"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+          </li>
+        </div>
       </div>
     </div>
   );
