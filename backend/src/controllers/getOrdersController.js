8b37eb3258657118eb3b7f7f3aaa80a36d3007b5
Lucia Lisdero <lulisdero20@gmail.com>
2023-07-06 15:06:14 -0300

    controller de orders y users

--- backend/src/controllers/getOrdersController.js
+++ backend/src/controllers/getOrdersController.js
@@ -1,12 +1,19 @@
 const { Order } = require("../db.js");
 
- getOrdersController = async() => {
-try{let orders = await Order.findAll();
-   return orders;
-}catch(error){
-throw new Error('Hubo un problema para encontrar las ordenes')
-}
-};
+getOrdersController = async(req) => {
+    try {
+      let orders;
+      if (req.query.id) {
+        orders = await Order.findOne({ where: { id: req.query.id } });
+      } else {
+        orders = await Order.findAll();
+      }
+      return orders;
+    } catch(error) {
+      throw new Error('Hubo un problema para encontrar las ordenes')
+    }
+  };
+  
 
 module.exports = {
     getOrdersController
