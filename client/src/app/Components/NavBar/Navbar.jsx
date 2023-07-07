d4e2cf8a204ff9bbba353de68786c727abc20212
claymartinez <clay.ms@hotmail.com>
2023-07-07 14:43:45 -0500

    Menu desplegable funcionando, pendiente modificar algo de css

--- client/src/app/Components/NavBar/Navbar.jsx
+++ client/src/app/Components/NavBar/Navbar.jsx
@@ -38,6 +38,7 @@ export default function Example() {
 
   const session = useSession();
 
+
   const autenticated = () => {
     if (session.status === 'authenticated') {
       return true;
@@ -99,40 +100,6 @@ export default function Example() {
                 </div>
               </div>
 
-              <div className="hidden sm:ml-6 sm:block">
-                <div className="flex space-x-4">
-                  {!autenticated() ? (
-                    <Link
-                      key="Login"
-                      href="/Login"
-                      className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-                    >
-                      Iniciar sesión
-                    </Link>
-                  ) : (
-                    <>
-                      <button onClick={() => setShowProfile(!showProfile)}>
-                        <p className="text-gray-300 rounded-md px-3 py-2 text-sm font-medium">
-                          {`Bienvenido ${
-                            session.data.user.name || session.data.user.username
-                          }`}
-                        </p>
-                      </button>
-
-                      {session.data.user?.image && (
-                        <Image
-                          className="hidden h-10 w-auto lg:block rounded-full"
-                          src={session.data.user.image}
-                          alt="image"
-                          width={100}
-                          height={100}
-                        />
-                      )}
-                    </>
-                  )}
-                </div>
-              </div>
-
               <div className="absolute inset-y-0 right-0 flex items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
                 {/* Profile dropdown */}
                 <Menu as="div" className="relative ml-3">
@@ -140,20 +107,42 @@ export default function Example() {
                     <Menu.Button className="flex rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
                       <span className="sr-only">Open user menu</span>
 
-                      <svg
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        strokeWidth={1.5}
-                        stroke="currentColor"
-                        className="w-6 h-6"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          d="M2.25 12.76c0 1.6 1.123 2.994 2.707 3.227 1.087.16 2.185.283 3.293.369V21l4.076-4.076a1.526 1.526 0 011.037-.443 48.282 48.282 0 005.68-.494c1.584-.233 2.707-1.626 2.707-3.228V6.741c0-1.602-1.123-2.995-2.707-3.228A48.394 48.394 0 0012 3c-2.392 0-4.744.175-7.043.513C3.373 3.746 2.25 5.14 2.25 6.741v6.018z"
-                        />
-                      </svg>
+                      <div className="hidden sm:ml-6 sm:block">
+                        <div className="flex space-x-4">
+                          {!autenticated() ? (
+                            <Link
+                              key="Login"
+                              href="/Login"
+                              className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
+                            >
+                              Iniciar sesión
+                            </Link>
+                          ) : (
+                            <>
+                              <button
+                                onClick={() => setShowProfile(!showProfile)}
+                              >
+                                <p className="text-gray-300 rounded-md px-3 py-2 text-sm font-medium">
+                                  {`Bienvenido ${
+                                    session.data.user.name ||
+                                    session.data.user.username
+                                  }`}
+                                </p>
+                              </button>
+
+                              {session.data.user?.image && (
+                                <Image
+                                  className="hidden h-10 w-auto lg:block rounded-full"
+                                  src={session.data.user.image}
+                                  alt="image"
+                                  width={100}
+                                  height={100}
+                                />
+                              )}
+                            </>
+                          )}
+                        </div>
+                      </div>
                     </Menu.Button>
                   </div>
                   <Transition
@@ -170,12 +159,18 @@ export default function Example() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                           >
-                            Facebook
+                            <svg
+                              class="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M2 10a8 8 0 018-8v8h8a8 8 0 11-16 0z"></path>
+                              <path d="M12 2.252A8.014 8.014 0 0117.748 8H12V2.252z"></path>
+                            </svg>
+                            <span class="ml-3">Dashboard</span>
                           </a>
                         )}
                       </Menu.Item>
@@ -183,12 +178,23 @@ export default function Example() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
+                            target="_blank"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                           >
-                            Instagram
+                            <svg
+                              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M5 3a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2V5a2 2 0 00-2-2H5zM5 11a2 2 0 00-2 2v2a2 2 0 002 2h2a2 2 0 002-2v-2a2 2 0 00-2-2H5zM11 5a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2V5zM11 13a2 2 0 012-2h2a2 2 0 012 2v2a2 2 0 01-2 2h-2a2 2 0 01-2-2v-2z"></path>
+                            </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">
+                              Kanban
+                            </span>
+                            <span class="inline-flex items-center justify-center px-2 ml-3 text-sm font-medium text-gray-800 bg-gray-200 rounded-full dark:bg-gray-700 dark:text-gray-300">
+                              Pro
+                            </span>
                           </a>
                         )}
                       </Menu.Item>
@@ -196,13 +202,121 @@ export default function Example() {
                         {({ active }) => (
                           <a
                             href="#"
-                            className={classNames(
-                              active ? 'bg-gray-100' : '',
-                              'block px-4 py-2 text-sm text-gray-700'
-                            )}
+                            target="_blank"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
                           >
-                            Linkedin
+                            <svg
+                              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path d="M8.707 7.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l2-2a1 1 0 00-1.414-1.414L11 7.586V3a1 1 0 10-2 0v4.586l-.293-.293z"></path>
+                              <path d="M3 5a2 2 0 012-2h1a1 1 0 010 2H5v7h2l1 2h4l1-2h2V5h-1a1 1 0 110-2h1a2 2 0 012 2v10a2 2 0 01-2 2H5a2 2 0 01-2-2V5z"></path>
+                            </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">
+                              Inbox
+                            </span>
+                            <span class="inline-flex items-center justify-center w-3 h-3 p-3 ml-3 text-sm font-medium text-blue-600 bg-blue-200 rounded-full dark:bg-blue-900 dark:text-blue-200">
+                              3
+                            </span>
                           </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                          >
+                            <svg
+                              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M10 9a3 3 0 100-6 3 3 0 000 6zm-7 9a7 7 0 1114 0H3z"
+                                clip-rule="evenodd"
+                              ></path>
+                            </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">
+                              Users
+                            </span>
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                          >
+                            <svg
+                              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                                clip-rule="evenodd"
+                              ></path>
+                            </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">
+                              Products
+                            </span>
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <a
+                            href="#"
+                            class="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                          >
+                            <svg
+                              class="flex-shrink-0 w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fill-rule="evenodd"
+                                d="M3 3a1 1 0 00-1 1v12a1 1 0 102 0V4a1 1 0 00-1-1zm10.293 9.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L14.586 9H7a1 1 0 100 2h7.586l-1.293 1.293z"
+                                clip-rule="evenodd"
+                              ></path>
+                            </svg>
+                            <span class="flex-1 ml-3 whitespace-nowrap">
+                              Sign In
+                            </span>
+                          </a>
+                        )}
+                      </Menu.Item>
+                      <Menu.Item>
+                        {({ active }) => (
+                          <button
+                            onClick={() => signOut()}
+                            className="flex items-center p-2 text-base font-normal text-gray-900 rounded-lg dark:text-white hover:bg-gray-100 dark:hover:bg-gray-700"
+                          >
+                            <svg
+                              className="w-6 h-6 text-gray-500 transition duration-75 dark:text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
+                              fill="currentColor"
+                              viewBox="0 0 20 20"
+                              xmlns="http://www.w3.org/2000/svg"
+                            >
+                              <path
+                                fillRule="evenodd"
+                                d="M10 2a4 4 0 00-4 4v1H5a1 1 0 00-.994.89l-1 9A1 1 0 004 18h12a1 1 0 00.994-1.11l-1-9A1 1 0 0015 7h-1V6a4 4 0 00-4-4zm2 5V6a2 2 0 10-4 0v1h4zm-6 3a1 1 0 112 0 1 1 0 01-2 0zm7-1a1 1 0 100 2 1 1 0 000-2z"
+                                clipRule="evenodd"
+                              ></path>
+                            </svg>
+                            <span className="flex-1 ml-3 whitespace-nowrap">
+                              Cerrar sesión
+                            </span>
+                          </button>
                         )}
                       </Menu.Item>
                     </Menu.Items>
@@ -211,11 +325,6 @@ export default function Example() {
               </div>
             </div>
           </div>
-
-          <div className="absolute top-16 right-44">
-            {showProfile && <ProfileUser />}
-          </div>
-
           {navigation.map(({ label, route }) => (
             <Disclosure.Panel className="sm:hidden" key={route}>
               <div className="bg-black space-y-1 px-2 pb-3 pt-2">
