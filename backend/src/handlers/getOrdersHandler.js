fe681c210bea2e9eeed0a0f09154e4ea36197292
Lucia Lisdero <lulisdero20@gmail.com>
2023-07-07 14:34:29 -0300

    le agregue el req a orderhandler

--- backend/src/handlers/getOrdersHandler.js
+++ backend/src/handlers/getOrdersHandler.js
@@ -2,7 +2,7 @@ const { getOrdersController } = require('../controllers/getOrdersController')
 
 const getOrdersHandler = async (req, res) => {
     try{
-     let orders = await getOrdersController();
+     let orders = await getOrdersController(req);
      res.status(200).json({ orders })
     }catch(error){
         res.status(400).json({ error: error.message })
