17c68d2056f1f4a1b187a4fcc00cab2f7bae024c
nicoargiz <nicoargiz@hotmail.com>
2023-07-07 09:32:14 -0300

    cambios en url de peticiones del front

--- client/src/app/FiltersPage/FilterComponents/ContainerF.jsx
+++ client/src/app/FiltersPage/FilterComponents/ContainerF.jsx
@@ -86,7 +86,7 @@ export default function ContainerF() {
   
     const handleClick = () => {
       axios
-        .post("http://localhost:3001/filter", filters)
+        .post("bienesraices-production-9eb3.up.railway.app/filter", filters)
         .then((res) => {
           if (res.data) {
             setPropiedad(res.data);
--- client/src/app/SignUp/page.jsx
+++ client/src/app/SignUp/page.jsx
@@ -43,7 +43,7 @@ export default function SignUp() {
 
     if (Object.keys(validationErrors).length === 0) {
       console.log(formData);
-      await axios.post('http://localhost:3001/users', formData);
+      await axios.post('bienesraices-production-9eb3.up.railway.app/users', formData);
       console.log('Signup successfully send');
       alert('Registro exitoso');
       setShowLogin(true);
