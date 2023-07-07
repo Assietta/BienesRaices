f58068298819354b1e1c3e320628179054da30ed
Lucia Lisdero <lulisdero20@gmail.com>
2023-07-07 14:41:23 -0300

    postCreateOrder

--- backend/src/handlers/postCreateOrderHandler.js
+++ backend/src/handlers/postCreateOrderHandler.js
@@ -1,13 +1,18 @@
 const { RealState } = require("../db");
 const mercadopago = require("mercadopago");
 require('dotenv').config();
+const { mailHandler } = require('./postMailHandler');
+const { User } = require("../db");
+const { Order } = require("../db");
+
 const {
-  TOKEN_MP
+  TOKEN_MP, HOST
 } = process.env;
 
 const createOrderHandler = async (req, res) => {
   const { id } = req.params;
   const userId = req.body.userId;
+  // console.log(userId)
 
   mercadopago.configure({
     access_token:
@@ -17,7 +22,11 @@ const createOrderHandler = async (req, res) => {
   try {
 
     const property = await RealState.findOne({ where: { id: id } });
+
+    // const price= parseFloat(property?.operations[0]?.prices[0]?.price) * 0.000001
+
     const price = parseFloat(property?.price) * 0.001;
+
     
     const result = await mercadopago.preferences.create({
       items: [
@@ -30,13 +39,15 @@ const createOrderHandler = async (req, res) => {
       ],
       
       back_urls: {
-        success: "http://localhost:3001/success",
-        failure: "http://localhost:3001/failure",
-        pending: "http://localhost:3001/pending",
+
+        success: `http://localhost:3001/success`,
+        failure: `http://localhost:3001/failure`,
+        pending: `http://localhost:3001/pending`,
       },
       auto_return:"approved",
       external_reference: userId,
       notification_url:'https://984b-190-174-229-190.ngrok-free.app/webhook'
+
     });
 
     const preferenceId = result.response.id;
@@ -53,9 +64,11 @@ const createOrderHandler = async (req, res) => {
     //   }
     // };
 
-    res.send(preferenceId);
-    // const redirectUrl = `https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=${preferenceId}`;
-    // res.send(redirectUrl);
+
+    // res.send(preferenceId);
+    const redirectUrl = `https://www.mercadopago.com.ar/checkout/v1/redirect?pref_id=${preferenceId}`;
+     res.send(redirectUrl);
+
   } catch (error) {
     console.error(error);
     res.status(500).send("Error al crear la orden");
@@ -66,53 +79,59 @@ const createOrderHandler = async (req, res) => {
 const webhookHandler=async(req, res)=>{
   // const payment= req.query.payment_id;
   const payment= req.query;
-  try {
-    if(payment.type==="payment"){
+  // console.log(payment);
+  try {  if(payment.type==="payment"){
       const data=await mercadopago.payment.findById(payment["data.id"]);
     console.log(data);
-
       // BUSCAR USER EN DB
-      const user = await User.findByPk(data.external_reference); // Asegúrate de que el modelo User exista y esté configurado correctamente
-
+      const user = await User.findByPk(data.response.external_reference); 
       // BUSCAR PROPERTY EN DB
-      const property = await RealState.findByPk(data.description); // Asegúrate de que el modelo RealState exista y esté configurado correctamente
+      const property = await RealState.findByPk(data.response.description); 
+// console.log(property);
 
       // GUARDAR REGISTRO EN ORDERS
       const newOrder = await Order.create({
-        client_idDB: data.external_reference,
-        usernameDB: user.username,
-        property_idDB: data.description,
-        address_of_property_to_reserveDB: property.address,
-        date_created: data.date_created,
-        chain: data.currency_id,
-        transaction_amount: data.transaction_amount,
-        date_approved: data.date_approved,
-        payment_id: data.id,
-        payer_first_nameMP: data.payer.first_name,
-        payer_last_nameMP: data.payer.last_name,
-        payer_emailMP: data.payer.email,
-        payer_emailDB: user.email,
-        payer_identificationMP: data.payer.identification,
-        payer_phoneMP: data.payer.phone,
-        payer_idMP: data.payer.id,
-        payment_method_id: data.payment_method_id,
-        payment_type_id: data.payment_type_id,
-        statement_descriptor_usernameMP: data.statement_descriptor,
-        status_approved_rejected: data.status,
-        status_detail_accredited: data.status_detail,
-        taxes_amount: data.taxes_amount,
-        transaction_amount_refunded: data.transaction_amount_refunded
+        client_idDB: String(data.response.external_reference),
+        usernameDB: String(user.dataValues.username),
+        property_idDB: String(data.response.description),
+        address_of_property_to_reserveDB: String(property.dataValues.address),
+        date_created: String(data.response.date_created),
+        chain: String(data.response.currency_id),
+        transaction_amount: String(data.response.transaction_amount),
+        date_approved: String(data?.response.date_approved),
+        payment_id: String(data?.response.id),
+        payer_first_nameMP: String(data?.response.payer['first_name']),
+        payer_last_nameMP: String(data?.response.payer['last_name']),
+        payer_emailMP: String(data?.response.payer['email']),
+        payer_emailDB: String(user?.dataValues.email),
+        payer_identificationMP: String(data?.response.payer['identification']),
+        payer_phoneMP: String(data?.response.payer['phone']),
+        payer_idMP: String(data?.response.payer['id']),
+        payment_method_id: String(data?.response.payment_method_id),
+        payment_type_id: String(data?.response.payment_type_id),
+        statement_descriptor_usernameMP: String(data?.response.statement_descriptor),
+        status_approved_rejected: String(data?.response.status),
+        status_detail_accredited: String(data?.response.status_detail),
+        taxes_amount: parseFloat(data?.response.taxes_amount),
+        transaction_amount_refunded: parseFloat(data?.response.transaction_amount_refunded),
       });
-
       console.log(newOrder);
-
+      let asunto='';
+      String(data?.response.status)==='approved'?  asunto='Su transacción ha sido exitosa': asunto='Su transacción ha sido rechazada';
+      const cuerpo = `Cualquier consulta comunicate con nosotros`
 
     //NOTIFICACION POR MAIL
+    await mailHandler(String(user?.dataValues.email), asunto, cuerpo);
 
-    res.status(200).send("OK");
+
+
+    res.status(204).send("OK");
     }
   } catch (error) {
-    return res.sendStatus(500).json({error: error.message});
+    console.log(error)
+    res.status(500).json({error: error.message});
+
+
   }
 };
 
@@ -136,24 +155,18 @@ module.exports = { createOrderHandler , webhookHandler};
 
 
 
+// try {
+//   const response = await axios.get(`https://api.mercadopago.com/v1/payments/${payment}`, {
+//       headers: {
+//           'Authorization': `Bearer TEST-4304274978344220-062414-3619cd7e5c484d5fcb746d26d1cc68c0-1406402853`
+//       }
+//   });
 
-  // try {
-  //   //   const data= await  axios.get(`https://api.mercadopago.com/v1/payments/${payment}`, {
-  //   //     headers: {
-  //   //         'Authorization': `Bearer TEST-4304274978344220-062414-3619cd7e5c484d5fcb746d26d1cc68c0-1406402853`
-  //   //     }
-  //   // }).then(response => {
-  //   //     console.log(response.data);
-  //   // }).catch(error => {
-  //   //     console.error(error);
-  //   // });
-  //   console.log(payment);
-    
-    
-  // } catch (error) {
-  //   console.log(error);
-  
-  // }
+//   console.log(response.data);
+// } catch (error) {
+//   console.error(error);
+// }
+
 
 
 
