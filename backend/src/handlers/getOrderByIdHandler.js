ce58cb68f0f4082d2eefd34c8d0a90e4943fc10c
nicoargiz <nicoargiz@hotmail.com>
2023-07-09 12:58:36 -0300

    ruta get Order por userId

--- backend/src/handlers/getOrderByIdHandler.js
+++ backend/src/handlers/getOrderByIdHandler.js
@@ -2,11 +2,11 @@ const { getOrderByIdController } = require('../controllers/getOrderByIdControlle
 
 const getOrderByIdHandler = async (req, res) => {
     try{
-    const userId = req.params;
-     let order = await getOrderByIdController(userId);
-     res.status(200).json({ order })
+    const { userId } = req.params;
+     const  order = await getOrderByIdController(userId);
+    return res.status(200).json( order )
     }catch(error){
-        res.status(400).json({ error: error.message })
+       return res.status(400).json({ error: error.message })
     }
   };
   
