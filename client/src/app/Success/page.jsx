e0d04aee6d8a73d6d3b14d1db91e4c71a1ea9777
Lucia Lisdero <lulisdero20@gmail.com>
2023-07-12 20:12:57 -0300

    de local a deploy

--- client/src/app/Success/page.jsx
+++ client/src/app/Success/page.jsx
@@ -25,7 +25,7 @@ export default function Success() {
             </h1>
             <div className="mt-10 flex items-center justify-center gap-x-6">
               <a
-                href="http://localhost:3000/"
+                href="https://bienesraices-production-888d.up.railway.app/"
                 className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
               >
                 MR Inicio
