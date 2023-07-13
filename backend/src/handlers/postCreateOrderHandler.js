ac1541a12e7f005a7af1b5d93de5994449e4be98
nicoargiz <nicoargiz@hotmail.com>
2023-07-13 12:57:29 -0300

    prueba deploy

--- backend/src/handlers/postCreateOrderHandler.js
+++ backend/src/handlers/postCreateOrderHandler.js
@@ -36,9 +36,9 @@ const createOrderHandler = async (req, res) => {
 
 
       back_urls: {
-        success: "https://bienesraices-production-888d.up.railway.app/Success",
-        failure: "https://bienesraices-production-888d.up.railway.app/Failure",
-        pending: "https://bienesraices-production-888d.up.railway.app/Pending",
+        success: "https://bienesraices-production-9eb3.up.railway.app/success",
+        failure: "https://bienesraices-production-9eb3.up.railway.app/failure",
+        pending: "https://bienesraices-production-9eb3.up.railway.app/pending",
       },
       auto_return: "approved",
       external_reference: userId,
