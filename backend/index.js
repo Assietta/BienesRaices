ec6741e2333ab41c6008283f072c571e300f58eb
nicoargiz <nicoargiz@hotmail.com>
2023-07-18 13:50:26 -0300

    veamos si asi funciona

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const { PORT } = process.env;
 
 
 // Syncing all the models at once.
-conn.sync({ alter: true }).then(() => {
+conn.sync({ force: true }).then(() => {
   server.listen(PORT, () => {
     createAdmin();
     getAllApi();
