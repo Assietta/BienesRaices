e5b763687bcd1313ab41448a951d7e17a62298e7
Lucia Lisdero <lulisdero20@gmail.com>
2023-07-18 14:04:54 -0300

    sin console.log  , alter true

--- backend/index.js
+++ backend/index.js
@@ -26,7 +26,7 @@ const { PORT } = process.env;
 
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ alter: true }).then(() => {
   server.listen(PORT, () => {
     createAdmin();
     getAllApi();
