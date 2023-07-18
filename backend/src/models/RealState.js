3232da3e3fdad28f070132406441b85b40c10c82
nicoargiz <nicoargiz@hotmail.com>
2023-07-18 14:34:48 -0300

    prueba sin Character varyng

--- backend/src/models/RealState.js
+++ backend/src/models/RealState.js
@@ -11,7 +11,7 @@ module.exports = (sequelize) => {
       primaryKey: true,
     },
     address: {
-      type: DataTypes.STRING(5000),
+      type: DataTypes.STRING,
       allowNull: false,
     },
     bathroom_amount: {
@@ -23,7 +23,7 @@ module.exports = (sequelize) => {
       allowNull: false,
     },
     description: {
-      type: DataTypes.STRING(3000),
+      type: DataTypes.STRING,
       allowNull: false,
     },
     disposition: {
