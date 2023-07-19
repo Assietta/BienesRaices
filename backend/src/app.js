db10e0d469ef4ecb6cf95785835cb4e472afe7e2
nicoargiz <nicoargiz@hotmail.com>
2023-07-19 13:09:39 -0300

    cors instalado

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const morgan = require('morgan');
@@ -7,7 +8,7 @@ const routes = require('./routes/index.js');
  require('./db.js');
 
 const server = express();
-
+server.use(cors());
 server.name = 'API';
 
 server.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
